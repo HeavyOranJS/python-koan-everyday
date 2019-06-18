f72d516d2d2c8baa11461063d3765a953eaa910a
HeavyJS <whereorange@gmail.com>
2019-06-18 17:57:08 +1000

    20_aboutScoringProject_twoSolutions

--- koans/about_scoring_project.py
+++ koans/about_scoring_project.py
@@ -32,9 +32,69 @@ from runner.koan import *
 #
 # Your goal is to write the score method.
 
+def naiveScore(dice):
+    finalScore = 0
+    numberCount = [0, 0, 0, 0, 0, 0]
+    #add score for 1's and 5's
+    for die in dice:
+        if die == 1:
+            finalScore += 100
+        elif die == 5:
+            finalScore += 50
+        numberCount[die-1] += 1
+    #cycle indexing
+    i = 0
+    #add score for any three number combination
+    for count in numberCount:
+        i += 1
+        if count >= 3:
+            #special case, because 1's give 100 points and 
+            #set of 1's awards more points
+            if i == 1:
+                finalScore += 700
+            #special case for 5
+            elif i == 5:
+                finalScore += 350
+            else:
+                finalScore += i * 100
+    return finalScore
+    
 def score(dice):
-    # You need to write this method
-    pass
+    #this solution should scale better, 
+    #because there is no list with all possible rolls to go through 
+
+    #trivial case
+    if (len(dice) == 0):
+        return 0
+
+    finalScore = 0
+    streak = 0
+    #first roll cannot count towards streak
+    lastDie = 0
+
+    #sorted order makes task easier: now all equial rolls are next to each other
+    sortedDice = sorted(dice)
+
+    for die in sortedDice:
+        if lastDie == die:
+            streak += 1
+            if streak == 2:#streak of 2 is three in a row
+                if die == 1:
+                    finalScore += 700
+                elif die == 5:
+                    finalScore += 350
+                else: finalScore += die * 100
+                streak = 0
+        else:
+            streak = 0
+
+        #awards for single 1 and 5 roll
+        if die == 1:
+            finalScore += 100
+        elif die == 5:
+            finalScore += 50
+        lastDie = die
+    return finalScore
 
 class AboutScoringProject(Koan):
     def test_score_of_an_empty_list_is_zero(self):
