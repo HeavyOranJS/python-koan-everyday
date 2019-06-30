3bf9127fcf3b0d3428d810d0e8e1ad2dcaf325f8
HeavyJS <whereorange@gmail.com>
2019-07-01 00:07:51 +1000

    aboutExtraCredit_mainGameCycle_needsDebugging

--- koans/about_extra_credit.py
+++ koans/about_extra_credit.py
@@ -18,14 +18,17 @@ from about_dice_project import DiceSet
 class Player():
     """Player class with player info. 
     By default should be empty, all variables are for testing purposes."""
-    def __init__(self, score = 0, scoreInRound = 0, rolledThisTurn = False,
-     gotEmtpyRoll = False, stoppedRolling = False, canEarnPoints = False, endGame = False):
+    def __init__(self, number = 1, score = 0, scoreInRound = 0, rolledThisTurn = False,
+     gotEmtpyRoll = False, stoppedRolling = False, canEarnPoints = False, endGame = False, dice = 5):
+        self.__number = number
         self._score = score
         self._scoreInRound = scoreInRound
         self._rolledThisTurn = rolledThisTurn
         self._gotEmptyRoll = gotEmtpyRoll #doesnt get to have points this turn
         self._stoppedRolling = stoppedRolling #player can stop after first roll
         self._canEarnPoints = canEarnPoints
+        self._diceCount = dice
+        self.currentDiceCount = self._diceCount
 
         self._endGame = endGame
 
@@ -37,6 +40,10 @@ class Player():
     def scoreInRound(self):
         return self._scoreInRound
 
+    @scoreInRound.setter
+    def scoreInRound(self, score):
+        self._scoreInRound = score
+
     @property
     def gotEmptyRoll(self):
         return self._gotEmptyRoll
@@ -45,30 +52,123 @@ class Player():
     def canEarnPoints(self):
         return self._canEarnPoints
 
-    def _roll(self, diceAmount):
-        self._values = [random.randint(1,6) for i in range(diceAmount)]
+    @property
+    def stoppedRolling(self):
+        return self._stoppedRolling
+
+    @stoppedRolling.setter
+    def stoppedRolling(self, value):
+        self._stoppedRolling = value
+
+    def getNumber(self):
+        return self.__number
+
+    def askIfWantToRollAgain(self):
+        """ask player if he wants to roll again on this round"""
+        print("Player " + str(self.__number) + ". Wanna roll again? Y/N")
+        answer = input()
+        while not (answer == "Y" or answer == "N"):
+            print("""Please, answer "Y"/"N" """)
+            answer = input()
+        if answer == "Y":
+            return True
+        return False
+
+    def _roll(self):
+        """returns list with amount of rolls equal to current avaliable dice"""
+        return [random.randint(1,6) for i in range(self.currentDiceCount)]
 
     def startTurn(self):
-        self._stoppedRolling = True
+        """get player ready for a new turn"""
+        self._stoppedRolling = False
+        self.currentDiceCount = self._diceCount
 
     def endTurn(self):
+        """apply scores and removie ability to roll"""
         if (self._canEarnPoints and not self._gotEmptyRoll): self._score += self._scoreInRound
+        #TODO add print if they havent rolled 300 in one turn yet
+        print("Player " + str(self.__number) + " has finished their turn with " +
+            str(self._scoreInRound) + " points and now has " +
+            str(self._score)+ " points.")        
         self._scoreInRound = 0
+        self.dice = 0
         
         self._stoppedRolling = True
 
-class Greed():
+class Greed:
     """Greed game. Rules at GREED_RULES.txt
-    Minimal amount of players is 2."""
-    def __init__(self, playerCount = 2, players = []):
+    Minimal amount of players is 2.
+    gameEndingPoints defines how many points is needed to trigger last round. Default is 3000"""
+    def __init__(self, playerCount = 2, gameEndingPoints = 3000, players = []):
         if playerCount > 2:
             raise ValueError("Minimal amount of players is 2. Your arg was " + str(playerCount)) 
         self._players = players
+        self._gameEndingPoints = gameEndingPoints
         for player in range(playerCount):
             self._players.append(Player())
 
-    def game(self):
-        pass
+    def _game(self):
+        print("Game has started.")
+        gameIsEnding = False
+        roundNumber = 0
+        while not gameIsEnding:
+            for player in self._players:
+                if player.score >= self._gameEndingPoints:
+                    gameIsEnding = True
+                    print("Player " + player.getNumber() + " has more than " +
+                        str(self._gameEndingPoints) +
+                        "! This round is your last chance to catch up!")
+                roundNumber += 1
+                self._round(roundNumber)
+        self._findWinner()
+        print("Game has ended.")
+
+    def _findWinner(self):
+        #TODO refactor this garbage
+        leaderBoard = []
+        leaderBoardDict = {}
+        for player in self._players:
+            leaderBoard.append(player.score)
+            leaderBoardDict["Player " + str(player.getNumber())] = player.score
+            print("Player " + str(player.getNumber()) + " got " + player.score + " points")
+        
+        leaderBoard.sort(reverse = True)
+        
+        #TODO whaaaaat is this massacre, there is no way use dict to find name of the winner is correct
+        print("Player " + leaderBoardDict.values(leaderBoard[0]) + " won with " + leaderBoard[0] + "points!")  
+
+    def _round(self, roundNumber):
+        #get all players ready for round
+        rollNumber = 0
+        for player in self._players:
+            player.startTurn()
+        print(self._players[0].stoppedRolling)
+        #everybody rolls first roll of the roundf
+        firstRoll = True
+        #start round
+        #while anyone still wants to roll
+        while any(player.stoppedRolling == False for player in self._players):
+            #get all players who havent stopped rolling
+            rollNumber += 1
+            print("-----Round " + str(roundNumber) + " Roll " + str(rollNumber) + "-----")
+            playersInGame = []
+            for player in self._players:
+                if player.stoppedRolling == False:
+                    #if this is not a first roll in round check if player wants to stop
+                    if firstRoll or player.askIfWantToRollAgain():
+                        print("Rolling...")
+                        #roll and find score
+                        score = self._score(player._roll())
+                        if not score == 0:
+                            player._scoreInRound += score
+                            print("Now your score in round is " + str(player._scoreInRound) + "and you have " + "TODO" + " dice left.")
+                        else:
+                            player.endTurn()
+                        player.scoreInRound += score
+                    else:
+                        player.stoppedRolling = True
+            firstRoll = False
+    #TODO print stats
 
     def isEndgame(self):
         for player in self._players:
@@ -82,7 +182,7 @@ class Greed():
     #could import this from about_scoring_project if wrap this in class
     def _score(self, dice):
         #trivial case
-        if (len(dice) < 0):
+        if (len(dice) < 1):
             raise ValueError("You tried rolling less than a die. UNACCEPTABLEEee")
 
         #first roll cannot count towards streak
@@ -109,6 +209,8 @@ class Greed():
             elif die == 5:
                 finalScore += 50
             lastDie = die
+        #TODO add spaces between numbers
+        print("You've rolled " + ''.join(str(roll) for roll in dice) + ", this equals to " + str(finalScore) + " points")
         return finalScore
 
 class AboutExtraCredit(Koan):
@@ -128,13 +230,13 @@ class AboutExtraCredit(Koan):
         sadPlayer.endTurn()
         self.assertEqual(0, sadPlayer.score)
         
-        sadPlayer.scoreInRound = 300
-        sadPlayer.endTurn()
-        self.assertEqual(300, sadPlayer.score)
+        luckyPlayer = Player(300)
+        luckyPlayer.endTurn()
+        self.assertEqual(300, luckyPlayer.score)
 
-        sadPlayer.scoreInRound = 1
-        sadPlayer.endTurn()
-        self.assertEqual(301, sadPlayer.score)
+        luckyPlayer._scoreInRound= 1
+        luckyPlayer.endTurn()
+        self.assertEqual(301, luckyPlayer.score)
 
 
     #TODO implement some sort of controlled rolling, 
